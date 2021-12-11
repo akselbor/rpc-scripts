713c2cb306fb5a24966e5c6cb625507185370611
Aksel Borgen <akselborgen@outlook.com>
2021-12-11 13:49:56 +0100

    First iteration on an installation script for the NTNU PCs

---
+++ windows/setup.sh
@@ -0,0 +1,29 @@
+# Download Python
+curl https://www.python.org/ftp/python/3.10.1/python-3.10.1-amd64.exe --output python-3.10.1-amd64.exe;
+# Install it to a predefined location
+./python-3.10.1-amd64.exe /passive InstallAllUsers=0 PrependPath=1 Include_test=0 TargetDir=$HOMEDRIVE$HOMEPATH\\python310;
+
+# Install virtualenv on the python installation
+$HOMEDRIVE$HOMEPATH\\python310\\python.exe --version
+$HOMEDRIVE$HOMEPATH\\python310\\python.exe -m pip install virtualenv
+
+# Remove any old version of rpc-slave, and clone the latest release
+rm -rf rpc-slave/
+git clone https://github.com/akselbor/rpc-slave.git
+
+# Move into the rpc-slave director
+cd rpc-slave
+
+# Create and activate a virtual environment
+$HOMEDRIVE$HOMEPATH\\python310\\python.exe -m virtualenv venv
+source ./venv/Scripts/activate
+
+# Install the requirements
+python --version
+pip install -r requirements.txt
+
+# Start the slave node using this as the master node
+python main.py --master http://10.24.24.160:5000
+
+# Left here during testing to keep the command prompt open
+read -p "test" hello
